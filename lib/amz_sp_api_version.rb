a0b307120dcae20df5f1976017c7f507365a96dd
Eric Jensen <ericcj@gmail.com>
2021-02-16 16:41:50 -0500

    fix model building

--- lib/amz_sp_api_version.rb
+++ lib/amz_sp_api_version.rb
@@ -1,3 +1,3 @@
 module AmzSpApi
-  VERSION = '0.1.6'
+  VERSION = '0.1.7'
 end
--- lib/api_client.rb
+++ lib/api_client.rb
@@ -236,7 +236,7 @@ module AmzSpApi
         end
       else
         # models, e.g. Pet
-        AmzSpApi.constants.map{|c| AmzSpApi.const_get(c)}.select{|sub| sub.kind_of?(Module)}.detect{|sub| sub.const_defined?(return_type)}.const_get(return_type)
+        AmzSpApi.constants.map{|c| AmzSpApi.const_get(c)}.select{|sub| sub.kind_of?(Module)}.detect{|sub| sub.const_defined?(return_type)}.const_get(return_type).build_from_hash(data)
       end
     end
 
